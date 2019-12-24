4197180aec2ee6006e2bd7dc9d9416f24a4aaf3c
Broden Wanner <broden.wanner@outlook.com>
2019-12-24 01:22:00 -0600

    Added server reset script

---
+++ scripts/reset-server.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Restart the Gunicorn process to pick up the changes to the django app
+sudo systemctl restart gunicorn
+
+# If you change Gunicorn socket or service files, reload the daemon and restart the process 
+sudo systemctl daemon-reload
+sudo systemctl restart gunicorn.socket gunicorn.service
+
+# Nginx server block configuration, test the configuration and then Nginx
+sudo nginx -t && sudo systemctl restart nginx
