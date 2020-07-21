828e27843620fa1ce6a4899ea2e09b41f120c849
Broden Wanner <me@brodenwanner.com>
2020-07-21 00:03:56 -0500

    Updated deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,23 +4,30 @@ cd ~/practicality
 
 # Pull the new source code
 git pull origin master
+echo "Pulled source code"
 
 # Activate virtual environment
 source venv/scripts/activate
+echo "Activated the virtual environment"
 
 # Ensure all dependencies are installed
-pip install -r requirements.txt
+pip3 install -r requirements.txt
+echo "Installed dependencies"
 
 # Run migrations
 cd ./practicality
-python manage.py makemigrations
-python manage.py migrate
+python3 manage.py makemigrations
+python3 manage.py migrate
+echo "Ran migrations"
 
 # Build frontend
 cd frontend/app && ionic build --prod
+echo "Built frontend"
 
 # Collect static files
 python manage.py collectstatic --noinput
+echo "Collected static files"
 
 # Restart the server
 source ../scripts/restart-server.sh
+echo "Server restarted"
