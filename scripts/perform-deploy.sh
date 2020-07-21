be9fed3d9cb0c8bc5d5811f8fd1912bac9ae1a93
Broden Wanner <me@brodenwanner.com>
2020-07-20 22:52:42 -0500

    Transitioned over to github actions

--- scripts/perform-deploy.sh
+++ scripts/perform-deploy.sh
@@ -1,2 +1,2 @@
 # ssh into the machine and execute the actual deploy script
-ssh -o "StrictHostKeyChecking no" -t $HOST_USERNAME@$HOST_IP 'cd ~/practicality; git pull origin; source scripts/deploy.sh'
+ssh -t $HOST_USERNAME@$HOST_IP 'cd ~/practicality; git pull origin; source scripts/deploy.sh'
