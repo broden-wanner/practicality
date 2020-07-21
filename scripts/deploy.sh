abb15b3e6d94cf8198a893c9db04a30f690bae71
Broden Wanner <me@brodenwanner.com>
2020-07-21 00:23:18 -0500

    Updated deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,25 +4,25 @@ cd ~/practicality
 
 # Pull the new source code
 git pull origin master
-echo "Pulled source code"
+echo -e "Pulled source code\n\n"
 
 # Activate virtual environment
 if [ ! -d "venv" ]; then
   python3 -m venv venv
 fi
 source venv/bin/activate
-echo "Activated the virtual environment"
+echo -e "Activated the virtual environment\n\n"
 
 # Ensure all dependencies are installed
 pip3 install --upgrade pip
 pip3 install -r requirements.txt
-echo "Installed dependencies"
+echo -e "Installed dependencies\n\n"
 
 # Run migrations
 cd ./practicality
 python3 manage.py makemigrations
 python3 manage.py migrate
-echo "Ran migrations"
+echo -e "Ran migrations\n\n"
 
 # Build frontend
 cd frontend/ionic-app 
@@ -33,7 +33,7 @@ echo -e "Built frontend\n\n"
 
 # Collect static files
 python3 manage.py collectstatic --noinput
-echo "Collected static files"
+echo -e "Collected static files\n\n"
 
 # Restart the server
 source ../scripts/restart-server.sh
