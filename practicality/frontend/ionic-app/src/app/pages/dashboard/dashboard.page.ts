72a743d1348eb6b1658e01f6227b45bab7ca57a1
Broden Wanner <me@brodenwanner.com>
2020-07-23 22:42:46 -0500

    Updated styles

--- practicality/frontend/ionic-app/src/app/pages/dashboard/dashboard.page.ts
+++ practicality/frontend/ionic-app/src/app/pages/dashboard/dashboard.page.ts
@@ -1,4 +1,6 @@
+declare var particlesJS: any;
 import { Component, OnInit } from '@angular/core';
+import { particleConfig } from 'src/app/shared/particles-config';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,4 +11,9 @@ export class DashboardPage implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+
+  ionViewDidEnter() {
+    // Set up the particle packgroud
+    particlesJS('particles-js', particleConfig);
+  }
 }
